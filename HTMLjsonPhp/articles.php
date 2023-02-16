19d69ad22e9a3fc587119f5176a13c1ba2dab724
Karel63 <76937128+Karel63@users.noreply.github.com>
2023-02-16 10:45:25 +0100

    Update articles.php

--- HTMLjsonPhp/articles.php
+++ HTMLjsonPhp/articles.php
@@ -1,5 +1,5 @@
 <?php
-$data = json_decode(file_get_contents("articles.json"), true)[filter_input(INPUT_GET, "a") ?? "kontakty"];
+$data = json_decode(file_get_contents("articles.json"), true)[filter_input(INPUT_GET, "a") ?? "kontakty"]; //$_GET['a']
 $tmplt = file_get_contents("page.html");
 foreach($data as $key => $val){
     $tmplt = str_replace("{" . $key . "}", $val, $tmplt);
